9024fe145ea4776fa585d795f4a1e59022838b29
root <islam.a.mustafa@gmail.com>
2024-04-22 14:26:14 -0600

    adding logger script

--- run_sample.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-./Examples/Monocular-Inertial/mono_inertial_euroc ./Vocabulary/ORBvoc.txt ./Examples/Monocular-Inertial/EuRoC.yaml ../../Dataset/MH_01_easy/ ./Examples/Monocular-Inertial/EuRoC_TimeStamps/MH01.txt  dataset-MH01_monoi
